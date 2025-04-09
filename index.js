6e04ca67054beae6ca1866736af452fa95321100
Pedro Andrade <pedrotrabalhos123@hotmail.com>
2025-04-08 23:12:31 -0300

    Começando nodeJs e modificando caminhos no html e css

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const porta = 3000;
+const localhost = "0.0.0.0";
+
+const app = express();
+
+app.use(express.static("./public"));
+
+app.listen(porta, localhost, () => {
+  console.log(`O servidor está disponível em http://${localhost}:${porta};`);
+});
