fdb323488e39f6e58972814a972f55cc08ade8e4
Pedro Andrade <pedrotrabalhos123@hotmail.com>
2025-04-09 22:58:35 -0300

    Finalizando a autenticação de usuário e senha, logout, interação com o formulário, importanto a session, e arrumando htmls.

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 import express from "express";
+import autenticar from "./secure/autenticar.js";
+import session from "express-session";
 
 const porta = 3000;
 const localhost = "0.0.0.0";
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "ch4v3s3cr3t4",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 30, // 30 minutos
+    },
+  })
+);
+
+app.get("/login", (requisicao, resposta) => {
+  resposta.redirect("/login.html");
+});
+
+app.get("/logout", (requisicao, resposta) => {
+  requisicao.session.destroy();
+  resposta.redirect("/login.html");
+});
+
+app.post("/login", (requisicao, resposta) => {
+  const usuario = requisicao.body.usuario;
+  const senha = requisicao.body.senha;
+  if (usuario === "admin" && senha === "admin") {
+    requisicao.session.autenticado = true;
+    resposta.redirect("/menu.html");
+  } else {
+    resposta.redirect("login.html");
+  }
+});
+
 app.use(express.static("./public"));
 
-app.use(express.static("./private"));
+app.use(autenticar, express.static("./private"));
 
 app.listen(porta, localhost, () => {
   console.log(`O servidor está disponível em http://${localhost}:${porta};`);
---
+++ secure/autenticar.js
@@ -0,0 +1,7 @@
+export default function autenticar(requisicao, resposta, next) {
+  if (requisicao.session.autenticado === true) {
+    next();
+  } else {
+    resposta.redirect("/login");
+  }
+}
