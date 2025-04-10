9732fea1c906cb8f083c93527152d45ccb9d167f
Pedro Andrade <pedrotrabalhos123@hotmail.com>
2025-04-09 21:31:39 -0300

    Alterando a página de cadastro para a de menu, adicionando html e css nas páginas de cadastro de partido e candidato, adicionando ./private no nodeJS

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 
 app.use(express.static("./public"));
 
+app.use(express.static("./private"));
+
 app.listen(porta, localhost, () => {
   console.log(`O servidor está disponível em http://${localhost}:${porta};`);
 });
